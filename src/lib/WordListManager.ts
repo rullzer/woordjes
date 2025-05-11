a4330cf753e4e2f55c95fddc6a90ffe4977065bd
Roeland Jago Douma <roeland@famdouma.nl>
2025-05-11 19:26:57 +0200

    Woordjes jenna

--- src/lib/WordListManager.ts
+++ src/lib/WordListManager.ts
@@ -1,4 +1,4 @@
-import jenna1 from './data/jenna-woordjes-engels-20250401.json'
+import jenna1 from './data/jenna-woordjes-20250513.json'
 import teske1 from './data/teske-woordjes-engels-20250507.json'
 import { WordList } from './WordList';
 import { WordPair } from './WordPair';
