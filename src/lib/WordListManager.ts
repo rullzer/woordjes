352f0be25a89c7ce8ea4a2a1e579e98191855fdd
Roeland Jago Douma <rullzer@users.noreply.github.com>
2025-10-02 20:42:42 +0200

    Woordjes engels teske

--- src/lib/WordListManager.ts
+++ src/lib/WordListManager.ts
@@ -1,5 +1,5 @@
 import teske1 from './data/teske-woordjes-20251009.json'
-import jenna1 from './data/jenna-woordjes-engels-20251002.json'
+import teske2 from './data/teske-woordjes-engels-20251029.json'
 import { WordList } from './WordList';
 import { WordPair } from './WordPair';
 
@@ -19,6 +19,6 @@ function parseImport(data: { name: string, id: string, words: { word: string, tr
 export function getWordLists(): WordList[] {
 	return [
 		parseImport(teske1),
-		parseImport(jenna1),
+		parseImport(teske2),
 	];
 }
